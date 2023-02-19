fd1afebd28b5ff7fc3c130d90dee806527969105
EvanJonQiu <EvanJonQiu@users.noreply.github.com>
2023-02-19 21:20:25 +0800

    update

--- 1_LED/User/main.c
+++ 1_LED/User/main.c
@@ -1,6 +1,8 @@
 #include "stm32f10x.h"
 #include "Delay.h"
 
+int use_register();
+
 int main(void) {
 	
 	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
@@ -12,14 +14,29 @@ int main(void) {
 	
 	GPIO_Init(GPIOA, &GPIO_InitStructure);
 	
-	//GPIO_ResetBits(GPIOA, GPIO_Pin_0);
-	//GPIO_ResetBits(GPIOA, GPIO_Pin_0);
-	//GPIO_WriteBit(GPIOA, GPIO_Pin_0, Bit_SET);
 	while (1) {
 		
 		GPIO_WriteBit(GPIOA, GPIO_Pin_0, Bit_RESET);
 		Delay_ms(500);
 		GPIO_WriteBit(GPIOA, GPIO_Pin_0, Bit_SET);
+		Delay_ms(500);
+	}
+}
+
+int use_register() {
+	/**
+	 *  use register to light led
+	 */
+	RCC->APB2ENR |= RCC_APB2Periph_GPIOA;
+	
+	GPIO_TypeDef* p = GPIOA;
+	p->CRL &= ~(0x0f << (4 * 0));
+	p->CRL |= 0x00000003;
+	
+	while (1) {
+		p->BRR = GPIO_Pin_0;
+		Delay_ms(500);
+		p->BSRR = GPIO_Pin_0;
 		Delay_ms(500);
 	}
 }
